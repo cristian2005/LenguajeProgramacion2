73569564171fea5d9dc84a23134732c88943a892
Cristian Herrera <cherrera@COSEFI.com>
2020-10-06 15:59:00 -0400

    Primer commit

---
+++ Back-end/ProyectoFinal/ProyectoFinal/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProyectoFinal.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        private  IConfiguration _configuration;
+        private  string myDb1ConnectionString = null;
+
+        //Entities
+        public DbSet<Departamento> Departamentos { get; set; }
+        public ApplicationDbContext()
+        {
+            myDb1ConnectionString = _configuration.GetConnectionString("myDb1");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(myDb1ConnectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
